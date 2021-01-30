7a7a3bc753a1a07bbf1482c66f6558ec508638ef
axcamz <61030878+axcamz@users.noreply.github.com>
2021-01-30 19:05:06 +0700

    Prepare for Atomic Design

---
+++ src/components/atoms/CheckBox/index.js
@@ -0,0 +1,13 @@
+const CheckBox = ({checked, label, id, onClick}) => {
+    return (
+        <div className="p-1 cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-600 rounded" onClick={onClick}>
+            <input 
+                type="checkbox" 
+                checked={checked} 
+                id={id}
+            /> <span>{label}</span>
+        </div>
+    )
+}
+
+export default CheckBox

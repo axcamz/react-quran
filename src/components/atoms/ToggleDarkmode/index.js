3fc5fee3b678697c21a7439750505a765af527c3
axcamz <61030878+axcamz@users.noreply.github.com>
2021-04-21 19:29:45 +0700

    Added Lottie Toggle animatin dark mode & bookmark

--- src/components/atoms/ToggleDarkmode/index.js
+++ src/components/atoms/ToggleDarkmode/index.js
@@ -1,18 +1,40 @@
 import { useContext } from "react"
 import { GlobalContext } from "../../../context"
 import useTheme from "../../../hooks/useTheme";
+import animationData from "../../../assets/lottie/light-dark-mode-button.json"
+import Lottie from "react-lottie";
 
 const ToggleDarkmode = () => {
     const {settingValue} = useContext(GlobalContext);
     const [settings, setSettings] = settingValue
     const { darkMode } = settings;
 
+    const defaultOptions = {
+        loop: false,
+        autoPlay: false,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+    };
+
     useTheme(darkMode)
 
+    // return (
+    //     <div
+    //         onClick={() => setSettings({...settings, darkMode: !darkMode})} 
+    //         className={`h-6 w-6 rounded-full transition duration-300 ${!settings.darkMode ? "bg-gray-800" : "bg-white"}`}>
+    //     </div>
+    // )
     return (
-        <div
-            onClick={() => setSettings({...settings, darkMode: !darkMode})} 
-            className={`h-6 w-6 rounded-full transition duration-300 ${!settings.darkMode ? "bg-gray-800" : "bg-white"}`}></div>
+        <div className="" onClick={() => setSettings({...settings, darkMode: !darkMode})} >
+            <Lottie
+                direction = {darkMode ? -1 : 1}
+                options={defaultOptions}
+                width={100}
+                height={100}
+            />
+        </div>
     )
 }
 
--- src/components/molecules/Card/index.js
+++ src/components/molecules/Card/index.js
@@ -2,30 +2,45 @@ import classNames from 'classnames'
 import { useContext, useState } from 'react'
 import { GlobalContext } from '../../../context'
 import { BookMark } from '../../Icons'
+import animationData from "../../../assets/lottie/bookmark-animation.json"
+import Lottie from 'react-lottie'
 
 const Card = ({name, lafadz, arti, index, onClick, ...res}) => {
     const {bookmarkList} = useContext(GlobalContext);
     const [bookmarked, setBookmarked] = bookmarkList;
     const isBookmarked = bookmarked.includes(index);
-    const [clicked, setClicked] = useState(isBookmarked);
+    const [active, setActive] = useState(isBookmarked);
 
     const handleBookmarked = () => {
-        if(clicked){
+        if(active){
             (bookmarked.splice(bookmarked.indexOf(index), 1))
             setBookmarked(bookmarked)
-            setClicked(false)
+            setActive(false)
         } else {
             setBookmarked([...bookmarked, index])
-            setClicked(true)
+            setActive(true)
         }
     }
+
+    const defaultOptions = {
+        loop: false,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+    };
 
     return (
         <div className={classNames("bg-white duration-300 relative dark:bg-gray-700 dark:text-gray-50 text-gray-900 h-28 md:h-36 p-3 rounded-md hover:shadow-lg transition-all font-poppins flex flex-col justify-between")}>
             <div className="flex justify-between">
                 <div className="flex justify-center items-center dark:bg-gray-600 bg-theme-200 text-sm lg:text-lg lg:w-8 lg:h-8 h-6 w-6 rounded p-1 font-semibold dark:text-white transition-all duration-300 text-theme-600">{index}</div>
-                <div className="cursor-pointer">
-                    <BookMark className="h-7 text-theme-600" fill={clicked ? "currentColor":"none"} onClick={() => handleBookmarked()}/>
+                <div className="cursor-pointer h-10 w-10 bg-blue-500" onClick={() => handleBookmarked()}>
+                    {/* <BookMark className="h-7 text-theme-600" fill={active ? "currentColor":"none"} onClick={() => handleBookmarked()}/> */}
+                    <Lottie
+                    speed={2}
+                        direction={active ? 1 : -1}
+                        options={defaultOptions}
+                    />
                 </div>
             </div>
             <div className="flex justify-between items-center">
